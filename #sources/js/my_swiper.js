3918bded08f03c0691f7d75a9d35a9af38cb5d90
ivanmalafeyev <ivan.malafeyev@gmail.com>
2020-12-30 17:20:53 +0400

    lots-slider adaptive

--- #sources/js/my_swiper.js
+++ #sources/js/my_swiper.js
@@ -1,8 +1,8 @@
-const mySwiper = new Swiper(".swiper-container", {
+const mySwiper = new Swiper(".main-slider__body", {
   // Optional parameters
   // direction: "vertical",
   loop: true,
-  autoHeight: true,
+  autoHeight: false,
 
   // If we need pagination
   // pagination: {
@@ -21,6 +21,48 @@ const mySwiper = new Swiper(".swiper-container", {
   // },
   breakpoints: {
     320: {},
-    768: {},
+    768: {
+      autoHeight: true,
+    },
+  },
+});
+
+const myLotsSwiper = new Swiper(".lots__slide", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  // autoHeight: false,
+  // slidesPerView: 1,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".lots-slider-next",
+    prevEl: ".lots-slider-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  // el: '.swiper-scrollbar',
+  // },
+  breakpoints: {
+    320: {
+      autoHeight: true,
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    768: {
+      autoHeight: false,
+      slidesPerView: 3,
+    },
+    975: {
+      slidesPerView: 3,
+    },
   },
 });
--- luxtrader/js/script.js
+++ luxtrader/js/script.js
@@ -194,11 +194,11 @@ menuIcon.addEventListener("click", function () {
   });
 });
 ;
-var mySwiper = new Swiper(".swiper-container", {
+var mySwiper = new Swiper(".main-slider__body", {
   // Optional parameters
   // direction: "vertical",
   loop: true,
-  autoHeight: true,
+  autoHeight: false,
   // If we need pagination
   // pagination: {
   //   el: '.swiper-pagination',
@@ -214,7 +214,45 @@ var mySwiper = new Swiper(".swiper-container", {
   // },
   breakpoints: {
     320: {},
-    768: {}
+    768: {
+      autoHeight: true
+    }
+  }
+});
+var myLotsSwiper = new Swiper(".lots__slide", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  // autoHeight: false,
+  // slidesPerView: 1,
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".lots-slider-next",
+    prevEl: ".lots-slider-prev"
+  },
+  // And if we need scrollbar
+  // scrollbar: {
+  // el: '.swiper-scrollbar',
+  // },
+  breakpoints: {
+    320: {
+      autoHeight: true,
+      slidesPerView: 1
+    },
+    500: {
+      slidesPerView: 2
+    },
+    768: {
+      autoHeight: false,
+      slidesPerView: 3
+    },
+    975: {
+      slidesPerView: 3
+    }
   }
 });
 ;
